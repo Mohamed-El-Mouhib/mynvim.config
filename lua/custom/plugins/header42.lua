9da0cb026db16ecf29cd51f082b6aa5272de3cc0
Mohamed El mouhib <163548207+Mohamed-El-Mouhib@users.noreply.github.com>
2025-08-02 13:28:16 +0100

    header doesn

--- lua/custom/plugins/header42.lua
+++ lua/custom/plugins/header42.lua
@@ -6,7 +6,7 @@ return {
     default_map = true, -- map <F1> to add/update header
     auto_update = true, -- update on save
     user = 'your_login', -- your 42 login
-    mail = 'your_login@student.1337.fr', -- or your student email
+    mail = 'your_login@student.1337.ma', -- or your student email
     -- additional options available
   },
   config = function(_, opts)
