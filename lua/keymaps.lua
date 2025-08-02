f0b9273c71a8491582e7242050525374103cd0e6
Mohamed El mouhib <163548207+Mohamed-El-Mouhib@users.noreply.github.com>
2025-08-02 13:39:53 +0100

    contains some of plugin keymaps for more readability

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,17 +1,7 @@
--- ************************************************************************** --
---                                                                            --
---                                                        :::      ::::::::   --
---   keymaps.lua                                        :+:      :+:    :+:   --
---                                                    +:+ +:+         +:+     --
---   By: mel-mouh <mel-mouh@student.1337.fr>        +#+  +:+       +#+        --
---                                                +#+#+#+#+#+   +#+           --
---   Created: 2025/07/18 19:05:33 by mel-mouh          #+#    #+#             --
---   Updated: 2025/07/18 19:05:36 by mel-mouh         ###   ########.fr       --
---                                                                            --
--- ************************************************************************** --
-
 vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
 vim.keymap.set('n', '<leader>lv', '<cmd>Telescope live_grep<CR>', { desc = 'Telescope Live Grep' })
 vim.keymap.set('n', '<leader>qn', '<cmd>NorminetteDisable<CR>', { desc = 'Disable Norm' })
 vim.keymap.set('n', '<leader>qm', '<cmd>NorminetteEnable<CR>', { desc = 'Enable Norm' })
 vim.keymap.set('n', '<leader>qc', '<cmd>CopilotChat<CR>', { desc = 'Talk to AI' })
+vim.keymap.set('n', '<C-S-Left>', '<cmd>vertical resize -5<CR>')
+vim.keymap.set('n', '<C-S-Right>', '<cmd>vertical resize +5<CR>')
