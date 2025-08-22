dd0539c926966636c20997ec3c5e94f1a36280d5
Mohamed El mouhib <huntermed15@gmail.com>
2025-08-22 13:33:24 +0100

    fixed buffe size if another buffer is closed

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,30 +1,41 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  branch = 'v3.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- optional
-    'MunifTanjim/nui.nvim',
-  },
-  config = function()
-    require('neo-tree').setup {
-      window = {
-        width = 30,
-        mappings = {
-          ['<cr>'] = 'open',
-          ['S'] = 'split_with_window_picker',
-          ['s'] = 'vsplit_with_window_picker',
-          ['t'] = 'open_tabnew',
-          ['H'] = 'toggle_hidden', -- Shift+H to toggle hidden files
-        },
-      },
-      filesystem = {
-        filtered_items = {
-          visible = false, -- Hide hidden files by default
-          hide_dotfiles = true,
-          hide_gitignored = true,
-        },
-      },
-    }
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- optional
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		-- Setup the autocmd for window resizing
+		vim.api.nvim_create_autocmd("WinClosed", {
+			callback = function()
+				vim.defer_fn(function()
+					vim.cmd("wincmd =")
+				end, 50)
+			end,
+		})
+
+		-- Setup neo-tree
+		require("neo-tree").setup({
+			close_if_last_window = true, -- Close Neo-tree if it is the last window
+			window = {
+				width = 30,
+				mappings = {
+					["<cr>"] = "open",
+					["S"] = "split_with_window_picker",
+					["s"] = "vsplit_with_window_picker",
+					["t"] = "open_tabnew",
+					["H"] = "toggle_hidden", -- Shift+H to toggle hidden files
+				},
+			},
+			filesystem = {
+				filtered_items = {
+					visible = false, -- Hide hidden files by default
+					hide_dotfiles = true,
+					hide_gitignored = true,
+				},
+			},
+		})
+	end,
 }
