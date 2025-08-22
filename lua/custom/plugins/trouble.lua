57959bae281e363c5a9db561db2c04015766dcff
Mohamed El mouhib <huntermed15@gmail.com>
2025-08-22 13:34:47 +0100

    simple update

--- lua/custom/plugins/trouble.lua
+++ lua/custom/plugins/trouble.lua
@@ -1,18 +1,13 @@
 return {
-  -- Some other plugin
-  'nvim-lualine/lualine.nvim',
-
-  -- Your trouble.nvim code is just another item in the list.
-  -- NO "return" here.
-  {
-    'folke/trouble.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('trouble').setup {}
-      -- your keymaps...
-    end,
-  },
-
-  -- Another plugin
-  'nvim-treesitter/nvim-treesitter',
+	-- Some other plugin
+	"nvim-lualine/lualine.nvim",
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("trouble").setup({})
+			-- your keymaps...
+		end,
+	},
+	"nvim-treesitter/nvim-treesitter",
 }
