64f77d136194e498ade3c1f935a976206c3effb9
Mohamed El mouhib <163548207+Mohamed-El-Mouhib@users.noreply.github.com>
2025-09-07 14:56:55 +0100

    Change user and mail to placeholders in header42.lua
    Updated user and mail fields to use placeholders.

--- lua/custom/plugins/header42.lua
+++ lua/custom/plugins/header42.lua
@@ -5,8 +5,8 @@ return {
 	opts = {
 		default_map = true, -- map <F1> to add/update header
 		auto_update = true, -- update on save
-		user = "mel-mouh", -- your 42 login
-		mail = "mel-mouh@student.1337.ma", -- or your student email
+		user = "your_login", -- your 42 login
+		mail = "your_login@student.1337.ma", -- or your student email
 		-- additional options available
 	},
 	config = function(_, opts)
